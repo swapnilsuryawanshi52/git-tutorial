951690dfcec23c2c012bf5a72b5d666bdcf33bfc
swapnilsuryawanshi52 <swapnilss1407@gmail.com>
2026-02-28 02:27:55 +0530

    version 6

--- src/main/java/com/swapnil/git/Bye.java
+++ src/main/java/com/swapnil/git/Bye.java
@@ -3,6 +3,6 @@ package com.swapnil.git;
 public class Bye {
     static void main(String[] args) {
         System.out.println("Bye");
-        System.out.println("Bye");
+        System.out.println("Version 6");
     }
 }
