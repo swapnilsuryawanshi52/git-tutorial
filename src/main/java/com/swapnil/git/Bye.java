8e401364b899055535eb43b78ca485380c81ba28
swapnilsuryawanshi52 <swapnilss1407@gmail.com>
2026-02-28 02:23:53 +0530

    verison 4

--- src/main/java/com/swapnil/git/Bye.java
+++ src/main/java/com/swapnil/git/Bye.java
@@ -2,6 +2,7 @@ package com.swapnil.git;
 
 public class Bye {
     static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Bye");
+        System.out.println("Bye");
     }
 }
--- src/main/java/com/swapnil/git/HelloWorld.java
+++ src/main/java/com/swapnil/git/HelloWorld.java
@@ -3,5 +3,6 @@ package com.swapnil.git;
 public class HelloWorld {
      static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World 2");
     }
 }
