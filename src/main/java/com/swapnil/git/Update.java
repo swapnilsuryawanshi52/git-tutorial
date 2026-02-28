0932e784bac4cb2e1e1aba7a130730fde67dce2b
swapnilsuryawanshi52 <swapnilss1407@gmail.com>
2026-02-28 19:31:31 +0530

    version 7 ssh

---
+++ src/main/java/com/swapnil/git/Update.java
@@ -0,0 +1,8 @@
+package com.swapnil.git;
+
+public class Update {
+    static void main(String[] args) {
+        System.out.println("Update SSH");
+        System.out.println("Version 8");
+    }
+}
